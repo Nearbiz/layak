7cfad78d0018ed1fab3da1b3d58c3a25660b1103
Fiz <hapizzz343@gmail.com>
2026-04-25 18:36:05 +0800

    amplify yml

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import Landing from "./pages/Landing"
 import WorkerApp from "./pages/WorkerApp"
 import LenderPortal from "./pages/LenderPortal"
 import TngAnalytics from "./pages/TngAnalytics"
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/worker/worker_kumar_001" replace />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/worker/:id" element={<WorkerApp />} />
         <Route path="/lender" element={<LenderPortal />} />
         <Route path="/tng" element={<TngAnalytics />} />
